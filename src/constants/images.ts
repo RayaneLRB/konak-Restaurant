e774d00c26db582aca5a3242377bf8f10f842bab
RayaneLRB <rayanelrb2001@gmail.com>
2026-06-15 19:32:33 +0100

    Add files via upload

---
+++ src/constants/images.ts
@@ -0,0 +1,59 @@
+export const IMAGES = {
+  // Hero & Ambiance
+  hero: 'https://images.pexels.com/photos/11196769/pexels-photo-11196769.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=1920',
+  heroAlt: 'https://images.pexels.com/photos/33798952/pexels-photo-33798952.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=1920',
+  rooftop: 'https://images.pexels.com/photos/37844216/pexels-photo-37844216.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  terrace: 'https://images.pexels.com/photos/14262609/pexels-photo-14262609.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  nightCity: 'https://images.pexels.com/photos/14471525/pexels-photo-14471525.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+
+  // Interior
+  interior1: 'https://images.pexels.com/photos/34429977/pexels-photo-34429977.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  interior2: 'https://images.pexels.com/photos/29000038/pexels-photo-29000038.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  interior3: 'https://images.pexels.com/photos/29000037/pexels-photo-29000037.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  interiorLighting: 'https://images.pexels.com/photos/35326200/pexels-photo-35326200.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  tableSetup: 'https://images.pexels.com/photos/27138849/pexels-photo-27138849.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+
+  // Dishes & Plats
+  dish1: 'https://images.pexels.com/photos/27612507/pexels-photo-27612507.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  dish2: 'https://images.pexels.com/photos/35546716/pexels-photo-35546716.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  dish3: 'https://images.pexels.com/photos/15671273/pexels-photo-15671273.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  dish4: 'https://images.pexels.com/photos/15671411/pexels-photo-15671411.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  dish5: 'https://images.pexels.com/photos/27612511/pexels-photo-27612511.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+
+  // Steak & Viandes
+  steak1: 'https://images.pexels.com/photos/18015000/pexels-photo-18015000.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  steak2: 'https://images.pexels.com/photos/8112966/pexels-photo-8112966.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  steak3: 'https://images.pexels.com/photos/13322847/pexels-photo-13322847.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+
+  // Desserts
+  dessert1: 'https://images.pexels.com/photos/12927134/pexels-photo-12927134.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  dessert2: 'https://images.pexels.com/photos/35622247/pexels-photo-35622247.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  dessert3: 'https://images.pexels.com/photos/29683251/pexels-photo-29683251.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  dessert4: 'https://images.pexels.com/photos/12124906/pexels-photo-12124906.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+
+  // Cocktails & Drinks
+  cocktail1: 'https://images.pexels.com/photos/15161548/pexels-photo-15161548.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  cocktail2: 'https://images.pexels.com/photos/36189461/pexels-photo-36189461.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  cocktail3: 'https://images.pexels.com/photos/17127748/pexels-photo-17127748.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  cocktail4: 'https://images.pexels.com/photos/27897590/pexels-photo-27897590.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+
+  // Coffee
+  coffee1: 'https://images.pexels.com/photos/1132586/pexels-photo-1132586.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  coffee2: 'https://images.pexels.com/photos/162886/coffee-glass-beverage-coffee-mug-162886.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+
+  // Brunch
+  brunch1: 'https://images.pexels.com/photos/1510691/pexels-photo-1510691.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  brunch2: 'https://images.pexels.com/photos/34307858/pexels-photo-34307858.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  brunch3: 'https://images.pexels.com/photos/29086310/pexels-photo-29086310.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+
+  // Menu Categories
+  entreesChaudes: 'https://images.pexels.com/photos/5602606/pexels-photo-5602606.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  entreesFroides: 'https://images.pexels.com/photos/27612507/pexels-photo-27612507.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  pasta: 'https://images.pexels.com/photos/33312977/pexels-photo-33312977.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  healthy: 'https://images.pexels.com/photos/9213888/pexels-photo-9213888.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  sushi: 'https://images.pexels.com/photos/37356430/pexels-photo-37356430.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  seafood: 'https://images.pexels.com/photos/566343/pexels-photo-566343.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  fish: 'https://images.pexels.com/photos/27588082/pexels-photo-27588082.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  pizza: 'https://images.pexels.com/photos/32293382/pexels-photo-32293382.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+  burger: 'https://images.pexels.com/photos/11022623/pexels-photo-11022623.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=800&w=1200',
+} as const;
